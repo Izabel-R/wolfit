d5c313af98dd8215da62dd1acb859137f3d10a43
Izabel-R <izabel@proworks.com>
2024-03-16 22:53:22 -0700

    Deleted Unecessary file

--- app/microservice.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def microservice_request(method, endpoint, **kwargs):
-    base_url = 'http://0.0.0.0:5001'  # Example base URL, adjust as needed
-    url = f"{base_url}{endpoint}"
-    response = requests.request(method, url, **kwargs)
-    return response.json(), response.status_code
